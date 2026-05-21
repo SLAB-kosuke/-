2ae6f2f10e008b96ca8acf4a67aa63ed8dc3bee7
助 <piece.time.1980@gmail.com>
2026-05-21 10:47:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -16,20 +16,15 @@ from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
 
-  apiKey: "ここにapiKey",
-
-  authDomain: "ここにauthDomain",
-
-  projectId: "ここにprojectId",
-
-  storageBucket: "ここにstorageBucket",
-
-  messagingSenderId: "ここにmessagingSenderId",
-
-  appId: "ここにappId"
-
+const firebaseConfig = {
+  apiKey: "AIzaSyBGeCs9-gsS66uCZ9HqEsbSqNv4_dOE5Bg",
+  authDomain: "family-calendar-38bf7.firebaseapp.com",
+  projectId: "family-calendar-38bf7",
+  storageBucket: "family-calendar-38bf7.firebasestorage.app",
+  messagingSenderId: "419708212606",
+  appId: "1:419708212606:web:2c93b424d3bd8c7387cf2f",
+  measurementId: "G-H72FRHK749"
 };
-
 
 /* Firebase開始 */
 
