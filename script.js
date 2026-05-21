f75c3e10be47df8d59ea16ab30bd368bc65e8f16
助 <piece.time.1980@gmail.com>
2026-05-21 13:36:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import {
+  getAuth,
+  signInAnonymously
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
   getFirestore,
   collection,
   addDoc,
