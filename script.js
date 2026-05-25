ecf40395f8f2279120f2dd4727912ae0bbb80bf7
助 <piece.time.1980@gmail.com>
2026-05-25 12:03:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,7 @@
 // Firebase
 // =========================
 
-import { initializeApp }
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { initializeApp }from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import {
   getFirestore,
