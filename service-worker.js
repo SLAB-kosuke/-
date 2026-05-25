9ba3746e2910a354806a6f2d99977ec5c1473d8b
助 <piece.time.1980@gmail.com>
2026-05-25 11:40:23 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,38 @@
-```javascript
-self.addEventListener('install', () => {
+
+// service-worker.js
+
+self.addEventListener("install", event => {
+
   self.skipWaiting();
+
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener("activate", event => {
+
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
+
+    caches.keys().then(keys => {
+
+      return Promise.all(
+
         keys.map(key => caches.delete(key))
-      )
-    )
+
+      );
+
+    })
+
   );
+
+  self.clients.claim();
+
 });
-```
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    fetch(event.request)
+
+  );
+
+});
