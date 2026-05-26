ca3abf692c5eb9688f38d1db1e5a8274a8f99783
助 <piece.time.1980@gmail.com>
2026-05-26 13:01:09 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,49 @@
+const CACHE_NAME = "wagaya-schedule-v2-cache-v1";
 
-// service-worker.js
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./script.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 
   self.skipWaiting();
-
 });
 
-self.addEventListener("activate", event => {
-
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-
-    caches.keys().then(keys => {
-
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-
-        keys.map(key => caches.delete(key))
-
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
       );
-
     })
-
   );
 
   self.clients.claim();
-
 });
 
-self.addEventListener("fetch", event => {
-
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-
-    fetch(event.request)
-
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).catch(() => {
+          return caches.match("./index.html");
+        })
+      );
+    })
   );
-
 });
